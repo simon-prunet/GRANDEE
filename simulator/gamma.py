4e16aff90ef1ba55b69f0431894d31be962dd4ba
Simon Prunet <prunet@gmail.com>
2025-02-25 13:31:02 +0000

    Simulator class, associated notebook for testing purposes

--- simulator/gamma.py
+++ simulator/gamma.py
@@ -1,10 +1,18 @@
 import numpy as np
 import scipy.special as ssp
 
-def gamma(x, a=4, scale=1):
-    return np.exp(-x/scale)/(ssp.factorial(a-1)*scale**a) * x**(a-1)
+def mygamma(x, a=4, scale=1, loc=0.):
+    res = np.zeros_like(x)
+    y = x-loc
+    res [y>0] = np.exp(-y[y>0]/scale)/(ssp.factorial(a-1)*scale**a) * y[y>0]**(a-1)
+    return res
 
-def dergamma(x, a=4, scale=1):
-    return ((a-1.)/x - 1./scale)*gamma(x,a,scale)
+def dergamma(x, a=4, scale=1, loc=0.):
+    y = x-loc
+    res = np.zeros_like(x)
+    res [y>0] = ((a-1.)/y[y>0] - 1./scale)*mygamma(y[y>0],a,scale)
+    return res
+
+
 
 
---
+++ simulator/simulator.py
@@ -0,0 +1,55 @@
+from torch.utils.data import Dataset
+import numpy as np
+from gamma import dergamma, mygamma
+import scipy.special as ssp
+
+class Simulator(Dataset):
+
+    def __init__(self, in_nc=3, n_samples=8192, a=4, 
+                 sigma_range=[0.1,10.], amplitude_range=[0.1,10.],
+                 scale_range=[1.,30.],loc_range=[100,8000],
+                 length=10000):
+
+
+        self.a = a # Gamma function derivative parameter
+        self.in_nc = in_nc # Number of channels
+        self.n_samples = n_samples # Number of samples per channel
+        self.sigma_range = sigma_range
+        self.amplitude_range = amplitude_range
+        self.scale_range = scale_range
+        self.loc_range = loc_range
+        self.length = length # Because datasets must have a length
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx, sigma_in=None):
+
+        loc = np.random.uniform(low=self.loc_range[0],high=self.loc_range[1])
+        scale = np.random.uniform(low=self.scale_range[0],high=self.scale_range[1])
+        amplitudes = np.random.uniform(low=self.amplitude_range[0],high=self.amplitude_range[1],size=self.in_nc)
+        # Typical peal
+        peak_scale = (self.a-1)**(self.a-1)/ssp.factorial(self.a-1)/scale * np.exp(-(self.a-1))
+        if sigma_in is not None:
+            sigma = sigma_in * peak_scale
+        else:
+            sigma = np.random.uniform (low=self.sigma_range[0],high=self.sigma_range[1]) * peak_scale
+
+
+        traces = np.zeros((self.in_nc, self.n_samples))
+        noisy = np.zeros((self.in_nc+1,self.n_samples))
+        for i in range(self.in_nc):
+            traces[i,:] = mygamma(np.arange(1,self.n_samples+1,dtype=float), a=self.a, scale=scale,loc=loc)*amplitudes[i]
+            noisy[i+1,:]= traces[i,:] + sigma*np.random.standard_normal(self.n_samples)
+        # Add noise amplitude in first line of noisy array
+        noisy[0,:] = sigma
+
+        return (noisy, traces)
+    
+
+
+
+
+
+
+
